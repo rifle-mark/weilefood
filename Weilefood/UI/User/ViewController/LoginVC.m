ac2b9d53539dbc7375da770703cc8c0661439e15
柯磊 <kelei0017@gmail.com>
2015-07-28 00:21:04 +0800

    完成登录界面配色

--- Weilefood/UI/User/ViewController/LoginVC.m
+++ Weilefood/UI/User/ViewController/LoginVC.m
@@ -20,7 +20,11 @@
 @property (nonatomic, strong) UIScrollView *scrollView;
 @property (nonatomic, strong) UIView       *contentView;
 
+@property (nonatomic, strong) UIView       *phoneBGView;
+@property (nonatomic, strong) UILabel      *phoneNameLabel;
 @property (nonatomic, strong) UITextField  *phoneTextField;
+@property (nonatomic, strong) UIView       *passwordBGView;
+@property (nonatomic, strong) UILabel      *passwordNameLabel;
 @property (nonatomic, strong) UITextField  *passwordTextField;
 @property (nonatomic, strong) UIButton     *resetPasswordButton;
 @property (nonatomic, strong) UIButton     *registerButton;
@@ -29,7 +33,9 @@
 @property (nonatomic, strong) UILabel      *hintLabel;
 @property (nonatomic, strong) UIView       *separateView;
 @property (nonatomic, strong) UIButton     *weiboLoginButton;
+@property (nonatomic, strong) UILabel      *weiboLabel;
 @property (nonatomic, strong) UIButton     *weixinLoginButton;
+@property (nonatomic, strong) UILabel      *weixinLabel;
 
 @end
 
@@ -45,7 +51,12 @@
     [self.view addSubview:self.scrollView];
     [self.scrollView addSubview:self.contentView];
     
+    
+    [self.contentView addSubview:self.phoneBGView];
+    [self.contentView addSubview:self.phoneNameLabel];
     [self.contentView addSubview:self.phoneTextField];
+    [self.contentView addSubview:self.passwordBGView];
+    [self.contentView addSubview:self.passwordNameLabel];
     [self.contentView addSubview:self.passwordTextField];
     [self.contentView addSubview:self.resetPasswordButton];
     [self.contentView addSubview:self.registerButton];
@@ -53,7 +64,9 @@
     [self.contentView addSubview:self.hintLabel];
     [self.contentView addSubview:self.separateView];
     [self.contentView addSubview:self.weiboLoginButton];
+    [self.contentView addSubview:self.weiboLabel];
     [self.contentView addSubview:self.weixinLoginButton];
+    [self.contentView addSubview:self.weixinLabel];
     
     [self.scrollView handleKeyboard];
 }
@@ -70,20 +83,40 @@
         make.width.equalTo(self.view);
     }];
     
-    [self.phoneTextField mas_remakeConstraints:^(MASConstraintMaker *make) {
+    
+    [self.phoneBGView mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.left.top.right.equalTo(self.contentView).insets(UIEdgeInsetsMake(15, 15, 0, 15));
         make.height.equalTo(@40);
     }];
-    [self.passwordTextField mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.phoneTextField.mas_bottom).offset(5);
-        make.left.right.height.equalTo(self.phoneTextField);
+    [self.phoneNameLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.phoneBGView).offset(15);
+        make.centerY.equalTo(self.phoneBGView);
     }];
+    [self.phoneTextField mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.phoneBGView).offset(75);
+        make.right.equalTo(self.phoneBGView).offset(-15);
+        make.centerY.height.equalTo(self.phoneBGView);
+    }];
+    [self.passwordBGView mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.phoneBGView);
+        make.top.equalTo(self.phoneBGView.mas_bottom).offset(5);
+        make.height.equalTo(self.phoneBGView);
+    }];
+    [self.passwordNameLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.passwordBGView).offset(15);
+        make.centerY.equalTo(self.passwordBGView);
+    }];
+    [self.passwordTextField mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.right.equalTo(self.phoneTextField);
+        make.centerY.height.equalTo(self.passwordBGView);
+    }];
+    
     [self.resetPasswordButton mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.passwordTextField.mas_bottom);
-        make.left.equalTo(self.phoneTextField);
+        make.left.equalTo(self.passwordBGView);
     }];
     [self.registerButton mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.phoneTextField);
+        make.left.equalTo(self.passwordBGView);
         make.top.equalTo(self.resetPasswordButton.mas_bottom).offset(15);
         make.height.equalTo(@40);
         make.width.equalTo(self.loginButton);
@@ -99,7 +132,7 @@
     }];
     [self.separateView mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.top.equalTo(self.hintLabel.mas_bottom).offset(5);
-        make.left.right.equalTo(self.passwordTextField);
+        make.left.right.equalTo(self.passwordBGView);
         make.height.equalTo(@1);
     }];
     [self.weiboLoginButton mas_remakeConstraints:^(MASConstraintMaker *make) {
@@ -107,11 +140,21 @@
         make.left.equalTo(self.separateView);
         make.width.equalTo(self.weixinLoginButton);
     }];
+    [self.weiboLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.weiboLoginButton.mas_bottom).offset(15);
+        make.centerX.equalTo(self.weiboLoginButton);
+        make.height.equalTo(@(self.weiboLabel.font.lineHeight));
+    }];
     [self.weixinLoginButton mas_remakeConstraints:^(MASConstraintMaker *make) {
         make.top.width.equalTo(self.weiboLoginButton);
         make.right.equalTo(self.separateView);
         make.left.equalTo(self.weiboLoginButton.mas_right).offset(15);
-        make.bottomMargin.equalTo(@-15);
+    }];
+    [self.weixinLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.weixinLoginButton.mas_bottom).offset(15);
+        make.centerX.equalTo(self.weixinLoginButton);
+        make.height.equalTo(@(self.weixinLabel.font.lineHeight));
+        make.bottomMargin.equalTo(@-20);
     }];
 }
 
@@ -218,19 +261,57 @@
     return _contentView;
 }
 
+- (UIView *)phoneBGView {
+    if (!_phoneBGView) {
+        _phoneBGView = [[UIView alloc] init];
+        _phoneBGView.backgroundColor = k_COLOR_WHITESMOKE;
+        _phoneBGView.layer.cornerRadius = 4;
+    }
+    return _phoneBGView;
+}
+
+- (UILabel *)phoneNameLabel {
+    if (!_phoneNameLabel) {
+        _phoneNameLabel = [[UILabel alloc] init];
+        _phoneNameLabel.text = @"用户名";
+        _phoneNameLabel.textColor = k_COLOR_DIMGRAY;
+        _phoneNameLabel.font = [UIFont systemFontOfSize:14];
+    }
+    return _phoneNameLabel;
+}
+
 - (UITextField *)phoneTextField {
     if (!_phoneTextField) {
         _phoneTextField = [[UITextField alloc] init];
-        _phoneTextField.placeholder = @"请输入手机号";
+        _phoneTextField.placeholder = @"请输入手机号码";
         _phoneTextField.keyboardType = UIKeyboardTypePhonePad;
+        _phoneTextField.font = [UIFont systemFontOfSize:14];
     }
     return _phoneTextField;
+}
+
+- (UIView *)passwordBGView {
+    if (!_passwordBGView) {
+        _passwordBGView = [[UIView alloc] init];
+        _passwordBGView.backgroundColor = k_COLOR_WHITESMOKE;
+        _passwordBGView.layer.cornerRadius = 4;
+    }
+    return _passwordBGView;
+}
+
+- (UILabel *)passwordNameLabel {
+    if (!_passwordNameLabel) {
+        _passwordNameLabel = [[UILabel alloc] init];
+        _passwordNameLabel.text = @"密码";
+        _passwordNameLabel.textColor = k_COLOR_DIMGRAY;
+        _passwordNameLabel.font = [UIFont systemFontOfSize:14];
+    }
+    return _passwordNameLabel;
 }
 
 - (UITextField *)passwordTextField {
     if (!_passwordTextField) {
         _passwordTextField = [[UITextField alloc] init];
-        _passwordTextField.placeholder = @"请输入密码";
         _passwordTextField.secureTextEntry = YES;
     }
     return _passwordTextField;
@@ -247,7 +328,11 @@
 
 - (UIButton *)registerButton {
     if (!_registerButton) {
-        _registerButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        _registerButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        _registerButton.layer.cornerRadius = 4;
+        _registerButton.backgroundColor = k_COLOR_THEME_NAVIGATIONBAR;
+        _registerButton.titleLabel.font = [UIFont systemFontOfSize:15];
+        [_registerButton setTitleColor:k_COLOR_THEME_NAVIGATIONBAR_TEXT forState:UIControlStateNormal];
         [_registerButton setTitle:@"注册" forState:UIControlStateNormal];
         [_registerButton addTarget:self action:@selector(_registerAction) forControlEvents:UIControlEventTouchUpInside];
     }
@@ -257,6 +342,10 @@
 - (UIButton *)loginButton {
     if (!_loginButton) {
         _loginButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        _loginButton.layer.cornerRadius = 4;
+        _loginButton.backgroundColor = k_COLOR_ORANGE;
+        _loginButton.titleLabel.font = [UIFont systemFontOfSize:15];
+        [_loginButton setTitleColor:k_COLOR_WHITE forState:UIControlStateNormal];
         [_loginButton setTitle:@"登录" forState:UIControlStateNormal];
         [_loginButton addTarget:self action:@selector(_loginAction) forControlEvents:UIControlEventTouchUpInside];
     }
@@ -266,7 +355,8 @@
 - (UILabel *)hintLabel {
     if (!_hintLabel) {
         _hintLabel = [[UILabel alloc] init];
-        _hintLabel.textColor = [UIColor grayColor];
+        _hintLabel.textColor = k_COLOR_DIMGRAY;
+        _hintLabel.font = [UIFont systemFontOfSize:14];
         _hintLabel.text = @"或用以下方式登录";
     }
     return _hintLabel;
@@ -275,27 +365,47 @@
 - (UIView *)separateView {
     if (!_separateView) {
         _separateView = [[UIView alloc] init];
-        _separateView.backgroundColor = [UIColor grayColor];
+        _separateView.backgroundColor = k_COLOR_WHITESMOKE;
     }
     return _separateView;
 }
 
 - (UIButton *)weiboLoginButton {
     if (!_weiboLoginButton) {
-        _weiboLoginButton = [UIButton buttonWithType:UIButtonTypeSystem];
-        [_weiboLoginButton setTitle:@"微博登录" forState:UIControlStateNormal];
+        _weiboLoginButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_weiboLoginButton setImage:[UIImage imageNamed:@"user_login_weibo"] forState:UIControlStateNormal];
         [_weiboLoginButton addTarget:self action:@selector(_weiboLoginAction) forControlEvents:UIControlEventTouchUpInside];
     }
     return _weiboLoginButton;
 }
 
+- (UILabel *)weiboLabel {
+    if (!_weiboLabel) {
+        _weiboLabel = [[UILabel alloc] init];
+        _weiboLabel.textColor = k_COLOR_DIMGRAY;
+        _weiboLabel.font = [UIFont systemFontOfSize:14];
+        _weiboLabel.text = @"微博登录";
+    }
+    return _weiboLabel;
+}
+
 - (UIButton *)weixinLoginButton {
     if (!_weixinLoginButton) {
-        _weixinLoginButton = [UIButton buttonWithType:UIButtonTypeSystem];
-        [_weixinLoginButton setTitle:@"微信登录" forState:UIControlStateNormal];
+        _weixinLoginButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_weixinLoginButton setImage:[UIImage imageNamed:@"user_login_wx"] forState:UIControlStateNormal];
         [_weixinLoginButton addTarget:self action:@selector(_weixinLoginAction) forControlEvents:UIControlEventTouchUpInside];
     }
     return _weixinLoginButton;
+}
+
+- (UILabel *)weixinLabel {
+    if (!_weixinLabel) {
+        _weixinLabel = [[UILabel alloc] init];
+        _weixinLabel.textColor = k_COLOR_DIMGRAY;
+        _weixinLabel.font = [UIFont systemFontOfSize:14];
+        _weixinLabel.text = @"微信登录";
+    }
+    return _weixinLabel;
 }
 
 @end
