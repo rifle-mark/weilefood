75ccd45ca5e6f5992067ebc82784fe89260d709e
柯磊 <kelei0017@gmail.com>
2015-08-02 10:57:52 +0800

    上传图片uploadImageWithImageData方法去掉type参数

--- Weilefood/Helper/ServerHelper/WLServerHelper+UploadImage.h
+++ Weilefood/Helper/ServerHelper/WLServerHelper+UploadImage.h
@@ -10,28 +10,13 @@
 
 @class WLUploadImageModel;
 
-/**
- *  服务端上传的图片类型
- */
-typedef NS_ENUM(NSInteger, WLServerUploadImageType){
-    /**
-     *  PNG图片
-     */
-    WLServerUploadImageTypePNG,
-    /**
-     *  JPEG图片
-     */
-    WLServerUploadImageTypeJPEG,
-};
-
 @interface WLServerHelper (UploadImage)
 
 /**
  *  上传图片
  *
- *  @param imageData 图片NSData数据
  *  @param callback  完成时回调
  */
-- (void)uploadImageWithImageData:(NSData *)imageData type:(WLServerUploadImageType)type callback:(void (^)(WLUploadImageModel *apiInfo, NSError *error))callback;
+- (void)uploadImageWithImageData:(NSData *)imageData callback:(void (^)(WLUploadImageModel *apiInfo, NSError *error))callback;
 
 @end
--- Weilefood/Helper/ServerHelper/WLServerHelper+UploadImage.m
+++ Weilefood/Helper/ServerHelper/WLServerHelper+UploadImage.m
@@ -9,15 +9,42 @@
 #import "WLServerHelper+UploadImage.h"
 #import "WLModelHeader.h"
 #import "WLDictionaryHelper.h"
+#import <SDWebImage/NSData+ImageContentType.h>
 
 @implementation WLServerHelper (UploadImage)
 
-- (void)uploadImageWithImageData:(NSData *)imageData type:(WLServerUploadImageType)type callback:(void (^)(WLUploadImageModel *apiInfo, NSError *error))callback {
+- (void)uploadImageWithImageData:(NSData *)imageData callback:(void (^)(WLUploadImageModel *apiInfo, NSError *error))callback {
+    if (!imageData) {
+        GCBlockInvoke(callback, nil, [NSError errorWithDomain:@"参数imageData不能为空" code:-1 userInfo:nil]);
+        return;
+    }
+    
+    NSString *mimeType = [NSData sd_contentTypeForImageData:imageData];
+    NSString *extName = nil;
+    if ([mimeType isEqualToString:@"image/jpeg"]) {
+        extName = @"jpg";
+    }
+    else if ([mimeType isEqualToString:@"image/png"]) {
+        extName = @"png";
+    }
+    else if ([mimeType isEqualToString:@"image/gif"]) {
+        extName = @"gif";
+    }
+    else if ([mimeType isEqualToString:@"image/tiff"]) {
+        extName = @"tiff";
+    }
+    else if ([mimeType isEqualToString:@"image/webp"]) {
+        extName = @"webp";
+    }
+    else {
+        GCBlockInvoke(callback, nil, [NSError errorWithDomain:@"不支持的图片文件格式" code:-2 userInfo:nil]);
+        return;
+    }
+    
     AFHTTPRequestOperationManager *manager = [self httpManager];
     manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
     [manager POST:kServerUploadImageUrl parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-        NSString *fileName = [NSString stringWithFormat:@"image.%@", type == WLServerUploadImageTypePNG ? @"png" : @"jpg"];
-        NSString *mimeType = [NSString stringWithFormat:@"image/%@", type == WLServerUploadImageTypePNG ? @"png" : @"jpeg"];
+        NSString *fileName = [NSString stringWithFormat:@"image.%@", extName];
         [formData appendPartWithFileData:imageData name:@"imgFile" fileName:fileName mimeType:mimeType];
     } success:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSDictionary *responseDic = [WLDictionaryHelper validModelDictionary:responseObject];
