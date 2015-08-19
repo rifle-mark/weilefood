ba4b86c1b50475e2af76d299d28cdb3d64b14c5c
柯磊 <kelei0017@gmail.com>
2015-08-19 23:47:13 +0800

    增加二级评论功能；增加UITextView+Placeholder扩展；

--- Weilefood/Helper/ServerHelper/WLServerHelper+Comment.m
+++ Weilefood/Helper/ServerHelper/WLServerHelper+Comment.m
@@ -11,7 +11,7 @@
 
 @implementation WLServerHelper (Comment)
 
-- (void)comment_addWithType:(WLCommentType)type refId:(NSUInteger)refId content:(NSString *)content parentId:(NSUInteger)parentId callback:(void (^)(WLApiInfoModel *apiInfo, NSError *error))callback {
+- (void)comment_addWithType:(WLCommentType)type refId:(long long)refId content:(NSString *)content parentId:(long long)parentId callback:(void (^)(WLApiInfoModel *apiInfo, NSError *error))callback {
     NSString *apiUrl = [self getApiUrlWithPaths:@[@"comment", @"add"]];
     NSDictionary *parameters = @{@"comment": @{@"Type": @(type),
                                                @"RefId": @(refId),
@@ -21,18 +21,18 @@
     [self httpPOST:apiUrl parameters:parameters callback:callback];
 }
 
-- (void)comment_deleteWithCommentId:(NSUInteger)commentId callback:(void (^)(WLApiInfoModel *apiInfo, NSError *error))callback {
+- (void)comment_deleteWithCommentId:(long long)commentId callback:(void (^)(WLApiInfoModel *apiInfo, NSError *error))callback {
     NSString *apiUrl = [self getApiUrlWithPaths:@[@"comment", @"delete"]];
     NSDictionary *parameters = @{@"commentid": @(commentId)};
     [self httpPOST:apiUrl parameters:parameters callback:callback];
 }
 
-- (void)comment_getListWithType:(WLCommentType)type refId:(NSUInteger)refId maxDate:(NSDate *)maxDate pageSize:(NSUInteger)pageSize callback:(void (^)(WLApiInfoModel *apiInfo, NSArray *apiResult, NSError *error))callback {
+- (void)comment_getListWithType:(WLCommentType)type refId:(long long)refId maxDate:(NSDate *)maxDate pageSize:(NSUInteger)pageSize callback:(void (^)(WLApiInfoModel *apiInfo, NSArray *apiResult, NSError *error))callback {
     NSString *apiUrl = [self getApiUrlWithPaths:@[@"comment", @"list", @(type), @(refId), @(1), @(pageSize), @([maxDate millisecondIntervalSince1970])]];
     [self httpGET:apiUrl parameters:nil resultItemsClass:[WLCommentModel class] callback:callback];
 }
 
-- (void)comment_getMyListWithType:(WLCommentType)type refId:(NSUInteger)refId maxDate:(NSDate *)maxDate pageSize:(NSUInteger)pageSize callback:(void (^)(WLApiInfoModel *apiInfo, NSArray *apiResult, NSError *error))callback {
+- (void)comment_getMyListWithType:(WLCommentType)type refId:(long long)refId maxDate:(NSDate *)maxDate pageSize:(NSUInteger)pageSize callback:(void (^)(WLApiInfoModel *apiInfo, NSArray *apiResult, NSError *error))callback {
     NSString *apiUrl = [self getApiUrlWithPaths:@[@"comment", @"mylist", @(type), @(refId), @(1), @(pageSize), @([maxDate millisecondIntervalSince1970])]];
     [self httpGET:apiUrl parameters:nil resultItemsClass:[WLCommentModel class] callback:callback];
 }
--- Weilefood/Model/WLCommentModel.h
+++ Weilefood/Model/WLCommentModel.h
@@ -30,22 +30,27 @@ typedef NS_ENUM(NSUInteger, WLCommentType) {
 @interface WLCommentModel : NSObject
 
 /// ID
-@property (nonatomic, assign) NSUInteger commentId;
-/// 头像
+@property (nonatomic, assign) long long commentId;
+/// 发布者头像
 @property (nonatomic, copy) NSString *avatar;
-/// 昵称
+/// 发布者ID
+@property (nonatomic, assign) long long userId;
+/// 发布者昵称
 @property (nonatomic, copy) NSString *nickName;
+/// 回复用户ID
+@property (nonatomic, assign) long long toUserId;
+/// 回复用户昵称
+@property (nonatomic, copy) NSString *toNickName;
 /// 内容
 @property (nonatomic, copy) NSString *content;
+/// 回复评论ID
+@property (nonatomic, assign) long long parentId;
 /// 评论时间
 @property (nonatomic, copy) NSDate *createDate;
-///
-@property (nonatomic, assign) NSUInteger userId;
-///
-@property (nonatomic, assign) NSUInteger parentId;
-///
-@property (nonatomic, assign) NSUInteger refId;
-///
+/// 评价对象ID
+@property (nonatomic, assign) long long refId;
+/// 评论对象类型
 @property (nonatomic, assign) WLCommentType type;
+
 
 @end
